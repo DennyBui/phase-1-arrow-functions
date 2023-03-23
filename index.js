33e22566b203f04edd0d44d7f72c89dc8a4ce464
DennyBui <dbuidmd2018@gmail.com>
2023-03-23 15:18:17 -0700

    arrow functions lab complete

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(divide){
+    return 2000 / 100;
+}
+const square = (parameter1) => parameter1 * parameter1;
+square(5);
+const add = (parameter3, parameter2) => parameter3 + parameter2;
+add(4,2);
